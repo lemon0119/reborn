9d96c695de9234227d54fac031ede074cbd98b1f
waanan <1018484601@qq.com>
2015-07-16 17:29:57 +0800

    asd

--- protected/views/admin/addClass.php
+++ protected/views/admin/addClass.php
@@ -1,46 +1,47 @@
- <script src="<?php echo JS_URL;?>jquery-form.js"></script>
-<script>
-    $(document).ready(function(){
-            $("div.span9").find("a").click(function(){
-                var url = $(this).attr("href");
-                //$(this).attr("href","#");
-                if(url.indexOf("index.php") > 0){
-                    $("#cont").load(url);
-                    return false;//阻止链接跳转
-                }
-            });
-            
-    var options = {   
-        target:'#cont',   // 需要刷新的区域 
-        //type:'post',
-        //dataType:'json',
-        //resetForm:false,
-       // timeout:10000
-    };
-
-    $("#myForm").submit(function(){ 
-          $(this).ajaxSubmit(options);   
-      // 为了防止普通浏览器进行表单提交和产生页面导航（防止页面刷新？）返回false   
-           return false;   
-      });  
-    });
-</script>
+<?php require 'classLstSideBar.php';?>
 <div class="span9">
-<div class="hero-unit">
-
-<h4>新建班级</h4>
-<form id="myForm" method="post" action="./index.php?r=admin/classLst&&action=add" onkeydown="if(event.keyCode==13){return false;}"> 
-班级名：<input type="text" name="className">
-<br><br>
-<input type="submit" name="submit" value="提交"> 
-</form>   
-<?php
-if(isset($shao))
-{
-    echo $shao;
-}
-?>
-
+<?php $courses=Course::model()->findall(); ?>
+    <h2>新建班级</h2>
+    <form id="myForm" method="post" class="form-horizontal" action="./index.php?r=admin/addClass&&action=add" onkeydown="if(event.keyCode==13){return false;}">
+        <fieldset>
+            <legend>填写信息</legend>
+            <div class="control-group">
+                    <label class="control-label" for="input01">班级名</label>
+                    <div class="controls">
+                            <input name="className" type="text" class="input-xlarge" id="input01"/>
+                    </div>
+            </div>
+            <div class="control-group">
+                    <label class="control-label" for="input02">选择课程</label>
+                    <div class="controls">
+                        <select name="courseID" style="width: 185px" id="input02">
+                            <option value="" selected="selected">请选择课程</option>
+                            <?php 
+                                foreach ($courses as $key => $value) {?>
+                            <option value="<?php echo $value['courseID']; ?>"><?php echo $value['courseName']; ?></option>     
+                            <?php  }?>
+                        </select>
+                    </div>
+            </div>
+            <div class="form-actions">
+            <button type="submit" class="btn btn-primary">添加</button> <a href="./index.php?r=admin/classLst" class="btn">取消</a>
+            </div>
+        </fieldset>
+    </form>   
 </div>
-</div>
 
+<script>     
+$("#myForm").submit(function(){
+    var userID = $("#input01")[0].value;
+    if(userID === ""){
+        alert('班级名称不能为空');
+        return false;
+    }
+   
+    var pass2 = $("#input02")[0].value;
+    if(pass2 === ""){
+        alert('请选择合适的课程');
+        return false;
+    }
+});
+</script>
