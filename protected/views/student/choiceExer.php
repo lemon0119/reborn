de1a4e01c40bc04cbe12f77dbf7b5894b14d5bee
tianyamignyue <lichao_mail@163.com>
2015-09-13 17:23:03 +0800

    fix exam

--- protected/views/student/choiceExer.php
+++ protected/views/student/choiceExer.php
@@ -49,11 +49,8 @@ $(document).ready(function(){
     $("li#li-choice").attr('class','active');
 });
 function formSubmit(){
-    $('#klgAnswer').submit();
-        /*
   $.post($('#klgAnswer').attr('action'),$('#klgAnswer').serialize(),function(result){
       alert(result);
   });
-  */
 }
 </script>
