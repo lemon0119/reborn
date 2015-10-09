d81cc3aa91c6a5beac51ee0de9d85ba0370ea312
FeiFei0620 <758787606@qq.com>
2015-10-09 11:34:54 +0800

    修改

--- protected/views/student/lookExer.php
+++ protected/views/student/lookExer.php
@@ -32,7 +32,9 @@
                 <tr>
                     <?php if($isExam){?>
                         <td width = '250px'>分数：<?php echo $exerOne['score']?></td>
+                        <?php if($isOver){?>
                         <td width = '250px'>总时间：<?php echo $strTime?></td>
+                        <?php }?>
                     <?php }?>
                     <td width = '250px'>计时：<span id="time">00:00:00</span></td>
                     <td width = '250px'>速度：<span id="wordps">0</span> 字/分</td>
