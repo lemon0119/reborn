df84850aabfc33a48fcc93b10bcec8e944de0359
FeiFei0620 <758787606@qq.com>
2015-10-09 11:11:33 +0800

    班级的学生列表，考试设置时间，设置，头像

--- protected/views/student/lookExer.php
+++ protected/views/student/lookExer.php
@@ -32,9 +32,7 @@
                 <tr>
                     <?php if($isExam){?>
                         <td width = '250px'>分数：<?php echo $exerOne['score']?></td>
-                        <?php if($isOver){?>
                         <td width = '250px'>总时间：<?php echo $strTime?></td>
-                        <?php }?>
                     <?php }?>
                     <td width = '250px'>计时：<span id="time">00:00:00</span></td>
                     <td width = '250px'>速度：<span id="wordps">0</span> 字/分</td>
