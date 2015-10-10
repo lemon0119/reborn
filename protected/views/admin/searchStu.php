186501d9aa5b5f2e220674127dec179cfc0667a9
jasonjcpeng <jasonjcpeng@hotmail.com>
2015-10-10 17:42:03 +0800

    fix bug

--- protected/views/admin/searchStu.php
+++ protected/views/admin/searchStu.php
@@ -6,25 +6,25 @@
 <table class="table table-bordered table-striped">
     <thead>
         <tr>
-            <th>学号</th>
-            <th>用户名</th>
-            <th>班级</th>
-            <th>操作</th>
+            <th class="font-center">学号</th>
+            <th class="font-center">用户名</th>
+            <th class="font-center">班级</th>
+            <th class="font-center">操作</th>
         </tr>
     </thead>
             <tbody>        
                 <?php foreach($stuLst as $model):?>
                 <tr>
-                    <td style="width: 125px"><?php echo $model['userID'];?></td>
-                    <td><?php echo $model['userName'];?></td>
-                    <td><?php if($model['classID']=="0"){
+                    <td class="font-center" style="width: 125px"><?php echo $model['userID'];?></td>
+                    <td class="font-center"><?php echo $model['userName'];?></td>
+                    <td class="font-center"><?php if($model['classID']=="0"){
                                     echo "无";
                     }
                                 else{$classID = $model['classID'];
                                     $sqlClass = TbClass::model()->find("classID = $classID");
                                 echo $sqlClass['className'];}
                         ?></td>
-                    <td>  
+                    <td class="font-center" style="width: 100px">  
                         <a href="./index.php?r=admin/infoStu&&id=<?php echo $model['userID'];?>&&name=<?php echo $model['userName'];?>&&class=<?php echo $model['classID'];?>&&flag=search"><img title="资料" src="<?php echo IMG_URL; ?>detail.png"></a>
                         <a href="./index.php?r=admin/editStu&&id=<?php echo $model['userID'];?>&&name=<?php echo $model['userName'];?>&&class=<?php echo $model['classID'];?>&&flag=search"><img title="编辑" src="<?php echo IMG_URL; ?>edit.png"></a>
                         <a href="#" onclick="dele(<?php $userID=$model['userID'];
