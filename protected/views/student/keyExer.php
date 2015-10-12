387eb897332e972d4d90ae51f8f6246cec7e9040
hongyunliu <1259886635@qq.com>
2015-10-12 18:05:13 +0800

    修改学生

--- protected/views/student/keyExer.php
+++ protected/views/student/keyExer.php
@@ -80,13 +80,14 @@
             var isover = setInterval(function(){
                 var time = getSeconds();
                 var seconds =<?php if($isExam) echo $exerOne['time']; else echo '0';?>;
-                if(time >= seconds){
+                if(time >= seconds &&second!=0){
                     clearInterval(isover);
                     doSubmit(true,function(){
                         window.location.href="index.php?r=student/clsexamOne&&suiteID=<?php echo Yii::app()->session['suiteID'];?>&&workID=<?php echo Yii::app()->session['workID']?>";
                     });
                     
                 }
+                
             },1000);
         }
         startParse();
--- protected/views/student/listenExer.php
+++ protected/views/student/listenExer.php
@@ -73,7 +73,7 @@
                 //console.log(time + "time");
                 var seconds = <?php if($isExam) echo $exerOne['time']; else echo '0';?>;
                 //console.log(seconds + "seconds");
-                if(time >= seconds){
+                if(time >= seconds &&second!=0){
                     clearInterval(isover);
                     doSubmit(true,function(){
                         window.location.href="index.php?r=student/clsexamOne&&suiteID=<?php echo Yii::app()->session['suiteID'];?>&&workID=<?php echo Yii::app()->session['workID']?>";
