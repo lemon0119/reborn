272bb93d720a377de93a657e9c5c66555301e5fa
jasonjcpeng <jasonjcpeng@hotmail.com>
2015-11-06 18:26:11 +0800

    FIX
    增加填空题的UI
    暂时屏蔽了删除课时的功能
    不能上传重复的PPT,VIDEO,MP3

--- protected/views/admin/pptLst.php
+++ protected/views/admin/pptLst.php
@@ -2,7 +2,9 @@
 <div class="span3">
     <div class="well" style="padding: 8px 0;">
         <ul class="nav nav-list">
-            <li ><a href="./index.php?r=admin/infoCourse&&courseID=<?php echo $courseID;?>&&courseName=<?php echo $courseName;?>&&createPerson=<?php echo $createPerson;?>"><i class="icon-align-left"></i> 课时列表</a></li>
+            <li>&nbsp;</li>
+            <a href="./index.php?r=admin/infoCourse&&courseID=<?php echo $courseID;?>&&courseName=<?php echo $courseName;?>&&createPerson=<?php echo $createPerson;?>"><button  class="btn_bigret"></button></a>
+            <li>&nbsp;</li>
         </ul>
     </div>
 </div>
