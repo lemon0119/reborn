42b18356f42ce4565866ac4cc6e3a142cbb065d9
jasonjcpeng <jasonjcpeng@hotmail.com>
2016-01-27 10:56:57 +0800

    FIX
    完善课堂动态切换当前练习统计页

--- protected/views/teacher/tableClassExercise4Analysis.php
+++ protected/views/teacher/tableClassExercise4Analysis.php
@@ -3,7 +3,7 @@
 <script src="<?php echo XC_Confirm; ?>js/xcConfirm.js" type="text/javascript" charset="utf-8"></script>
 <script src="<?php echo JS_URL; ?>jquery.min.js"></script>
 <body style="background-image: none;background-color: #fff">
-    <div style="overflow: hidden" id="title"><h3 id="hh" class="fl"><?php echo $ClassExercise['title'];?></h3><button style="margin: 10px"  class="fr btn" id="back">返回上级</button><button onclick="next()" style="margin: 10px" id="next" class="fr btn btn-primary" <?php if(count($AllIsOpen)<2){echo 'disabled="disabled"';}?>>下一题</button><button class="fr btn btn-primary" onclick="last()" id="last" style="margin: 10px;" disabled="disabled">上一题</button></div>
+    <div style="overflow: hidden" id="title"><h3 id="hh" class="fl"><?php echo $ClassExercise['title'];?></h3><button style="margin: 10px"  class="fr btn" id="back">返回上级</button><button onclick="next()" style="margin: 10px" id="next" class="fr btn btn-primary" <?php if(count($AllIsOpen)<1){echo 'disabled="disabled"';}?>>下一题</button><button class="fr btn btn-primary" onclick="last()" id="last" style="margin: 10px;" disabled="disabled">上一题</button></div>
     <div style="overflow: auto;width: 98%;height: 420px;position: relative;">
         <table id="table_of_analysis"  class="table table-bordered table-striped">
          <thead>
@@ -24,14 +24,17 @@
     </div>
 </body>
 <script>
-    var Htitle = "";
-    var count = 1;
+    var flag = 0;
+    var count = 0;
     var  exericseID= <?php echo $_GET['exerciseID'];?>;
     var  allExercise = new Array();
     <?php foreach ($AllIsOpen as $k=>$v){ ?>
             allExercise[0] = exericseID;               
-            allExercise[<?php echo $k;?>] = <?php echo $v['exerciseID'];?>;
+            allExercise[<?php echo $k;?>+1] = <?php echo $v['exerciseID'];?>;
     <?php } ?>
+//    for(){
+//        
+//    }
     
     $(document).ready(function(){
        checkAnalysis(exericseID);
@@ -56,7 +59,18 @@
                        var newRow='<tr id="option'+i+'"><td >'+(i+1)+'</td><td >'+data[i]['studentID']+'</td><td >'+data[i]['studentName']+'</td><td>'+data[i]['speed']+'</td><td>'+data[i]['maxSpeed']+'</td><td>'+data[i]['correct']+'%</td><td>'+data[i]['time']+'</td><td>'+data[i]['allKey']+'</td></tr>';  
                        $('#table_of_analysis').append(newRow); 
                    }
-                  Htitle = data[0]['title'];   
+                   if(flag==1){
+                       var father = document.getElementById("title");
+                        var back = document.getElementById("back");
+                        var title = data[0]['title']; 
+                        var hh = document.createElement("h3");
+                        hh.setAttribute("id","hh");
+                        hh.setAttribute("class","fl");
+                        hh.innerHTML = title;
+                        father.insertBefore(hh,back);
+                        flag = 0;
+                   }
+                    
                },
                error:function(xhr, type, exception){
                    console.log('GetAverageSpeed error', type);
@@ -70,22 +84,18 @@
     }
     
     function next(){
+        flag =1 ;
         var h = document.getElementById("hh");
         var father = document.getElementById("title");
         father.removeChild(h);
-        var back = document.getElementById("back");
-        var title = Htitle;
-        var hh = document.createElement("h3");
-        hh.setAttribute("id","hh");
-        hh.setAttribute("class","fl");
-        hh.innerHTML = title;
-        father.insertBefore(hh,back);
-        if(count<allExercise.length){
+        
+        if(count<allExercise.length-1){
             count++;
         }
         exericseID = allExercise[count];
+        console.log("c"+count+"c"+exericseID);
         checkAnalysis(exericseID);
-        if(count == allExercise.length){
+        if(count == allExercise.length-1){
             $("#next").attr("disabled","disabled"); 
         }
         if(exericseID!=<?php echo $_GET['exerciseID'];?>){
@@ -94,6 +104,32 @@
            $("#last").attr("disabled","disabled"); 
         }
     }   
+    
+    function last(){
+        flag =1 ;
+        var h = document.getElementById("hh");
+        var father = document.getElementById("title");
+        father.removeChild(h);
+        
+        if(count>0){
+            count--;
+        }
+        exericseID = allExercise[count];
+        console.log("c"+count+"c"+exericseID);
+        checkAnalysis(exericseID);
+        if(count == 0){
+            $("#next").attr("disabled","disabled"); 
+        }
+        if(count != allExercise.length-1){
+            $("#next").removeAttr("disabled"); 
+        }
+        if(exericseID!=<?php echo $_GET['exerciseID'];?>){
+           $("#last").removeAttr("disabled");
+        }else{
+           $("#last").attr("disabled","disabled");
+           
+        }
+    }  
      
         
 </script>
