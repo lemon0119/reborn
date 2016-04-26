efd0bf6364324d398f858decf0b403ad4a6e8cc8
jasonjcpeng <jasonjcpeng@hotmail.com>
2016-04-26 17:26:47 +0800

    FIX

--- js/exerJS/GetAccuracyRate.js
+++ js/exerJS/GetAccuracyRate.js
@@ -3,74 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function max(p1, p2) {
-    return p1 > p2 ? p1 : p2;
-}
-function LCS(str1, str2) {
-    if (str1 == null || str2 == null)
-        return null;
-    this.substringLength1 = str1.length;
-    this.substringLength2 = str2.length;
-    this.subLCS = '';
-    this.opt = Array([this.substringLength1]);
-    for (var it = 0; it < this.substringLength1 + 1; ++it) {
-        this.opt[it] = new Array([this.substringLength2]);
-        for (var jt = 0; jt < this.substringLength2 + 1; ++jt) {
-            this.opt[it][jt] = 0;
+ function lcs(str1, str2) {
+        var arr = [];
+        //array init
+        for (var i = 0; i < str1.length + 1; i++) {
+            arr[i] = [];
+            for (var j = 0; j < str2.length + 1; j++) {
+                arr[i][j] = 0;            
+            }
+        }
+        for (var i = 1; i < str1.length + 1; i++) {
+            for (var j = 1; j < str2.length + 1; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    arr[i][j] =  arr[i - 1][j - 1] + 1;
+                } else if (arr[i - 1][j] >= arr[i][j - 1]) {
+                    arr[i][j] = arr[i - 1][j];
+                } else {
+                    arr[i][j] = arr[i][j - 1];
+                }
+            }
+        }
+        //正确的字符，以数组形式展示
+        var result = [];
+        _lcs(str1, str2, str1.length, str2.length, arr, result);
+        return result;
+    }
+
+    function _lcs(str1, str2, i, j, arr, result) {
+        if (i == 0 || j == 0) {
+            return;
+        }
+        if (str1[i - 1] == str2[j - 1]) {
+            _lcs(str1, str2, i - 1, j - 1, arr, result);
+            result.push(str1[i - 1]);
+        } else if (arr[i][j - 1] >= arr[i - 1][j]) {
+            _lcs(str1, str2, i, j - 1, arr, result);
+        } else {
+            _lcs(str1, str2, i - 1, j, arr, result);
         }
     }
-    this.x = str1;
-    this.y = str2;
-    this.x_m = "";
-    this.y_m = "";
-    this.doLCS = function () {
-        for (var i = this.substringLength1 - 1; i >= 0; i--) {
-            for (var j = this.substringLength2 - 1; j >= 0; j--) {
-                if (this.x[i] == this.y[j]) {
-                    this.opt[i][j] = this.opt[i + 1][j + 1] + 1;
-                }
-                else {
-                    this.opt[i][j] = max(this.opt[i + 1][j], this.opt[i][j + 1]);
-                }
-            }
-        }
-        i = 0;
-        j = 0;
-        while (i < this.substringLength1 && j < this.substringLength2) {
-            if (this.x[i] === this.y[j]) {
-                this.subLCS += this.x[i];
-                this.x_m += this.x[i];
-                this.y_m += this.x[i];
-                i++;
-                j++;
-            } else if (this.opt[i + 1][j] >= this.opt[i][j + 1]) {
-                this.x_m += '*';
-                i++;
-            } else {
-                this.y_m += '*';
-                j++;
-            }
-        }
-    };
-    this.getSubString = function (type) {
-        switch (type) {
-            case 1 :
-                return this.x_m;
-            case 2:
-                return this.y_m;
-            default:
-                return this.subLCS;
-        }
-    };
-    this.getStrOrg = function (type) {
-        switch (type) {
-            case 1 :
-                return this.x;
-            default:
-                return this.y;
-        }
-    };
-}
 
 
 onmessage = function (event) {
@@ -78,10 +49,9 @@ onmessage = function (event) {
     var originalContent = event.data.originalContent;
     var allCount = 0;
     var rightCount = 0;
-    var lcs = new LCS(currentContent, originalContent);
-    lcs.doLCS();
-    allCount = lcs.getStrOrg(1).length;
-    rightCount = lcs.getSubString(3).length;
+    var result = lcs(currentContent,originalContent);
+    allCount = originalContent.length;
+    rightCount = result.length;
     var correct = rightCount / allCount;
     var accuracyRate = Math.round(correct * 100);
     postMessage({
