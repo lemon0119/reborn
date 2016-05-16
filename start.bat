67d6e1a3f8399c635a2cd2251448f477de7be390
jasonjcpeng <jasonjcpeng@hotmail.com>
2016-05-16 14:24:03 +0800

    FIX
    start 提升权限

--- start.bat
+++ start.bat
@@ -1,3 +1,22 @@
+@echo off
+:--------------------------------------
+:: BatchGetAdmin
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto GetAdmin )
+:UACPrompt
+if not "%~1"=="" set file= ""%~1""
+(echo Set UAC = CreateObject("Shell.Application"^)
+echo UAC.ShellExecute "cmd.exe", "/c %~s0%file%", "", "runas", 1)> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:GetAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:--------------------------------------
+:StartCommand
 cd /d %~dp0
 cd "./protected/extensions/signaler"
 start /min node server-side.js
